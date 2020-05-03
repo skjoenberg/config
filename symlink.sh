38892f59e3cb7ce566b62281d4b2b5962ddefdd5
Sebastian Andersen <sebastian@skjoenberg.com>
2020-05-03 10:00:55 +0200

    Some requires copying

--- symlink.sh
+++ symlink.sh
@@ -1,1 +1,13 @@
-ln -s xbindkeys/.xbindkeysrc ~/
+create_symlink () {
+    rm -f $2
+    ln -s $1 $2 
+}
+
+copy () {
+    rm -f $2
+    cp $1 $2 
+}
+
+copy xbindkeys/.xbindkeysrc ~/.xbindkeysrc
+copy bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+copy sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
